ecd05392ef0efa639221163e1166b99e38d345a6
Leszek <smutnyleszek@gmail.com>
2024-01-09 01:15:03 +0100

    run prettier over things

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  bracketSpacing: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
